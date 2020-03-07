25421f38adf5583d556baf930dec4edcf5ccfc4c
Paul Matthew Barrameda <luisanduis729@gmail.com>
2020-03-07 01:32:19 -0800

    Refactor and reduce test time of UpdateHandler

--- test/UpdateHandler.test.ts
+++ test/UpdateHandler.test.ts
@@ -20,12 +20,9 @@ describe('UpdateHandler', () => {
         () => false,
       );
 
-      assert(
-        updateHandler.handlers.size === 1,
-        `map size should be 1, found ${updateHandler.handlers.size}`,
-      );
-      assert(
-        updateHandler.handlers.get('status') !== undefined,
+      assert.equal(updateHandler.handlers.size, 1);
+      assert.isDefined(
+        updateHandler.handlers.get('status'),
         'missing value for "status" in the updateHandler',
       );
       assert.deepEqual(
@@ -37,10 +34,7 @@ describe('UpdateHandler', () => {
 
     it('should have no handler when none is added', () => {
       updateHandler = new UpdateHandler();
-      assert(
-        updateHandler.handlers.size === 0,
-        'map size should be 0 as no handlers have been added',
-      );
+      assert.equal(updateHandler.handlers.size, 0);
     });
 
     it('should add an expiring handler properly', (done) => {
@@ -53,14 +47,14 @@ describe('UpdateHandler', () => {
       );
 
       setTimeout(() => {
-        assert(counter === 1, `counter should be 1 after handler expires, is ${counter}`);
+        assert.equal(counter, 1, `counter should be 1 after handler expires, is ${counter}`);
         assert.deepEqual(
           updateHandler.handlers.get('status'),
           [],
           `array in "status" is not empty, is ${updateHandler.handlers.get('status')}`,
         );
         done();
-      }, 50);
+      }, 20);
     });
   });
 
@@ -72,15 +66,15 @@ describe('UpdateHandler', () => {
         }
       }, (v) => v === 10);
 
-      assert(counter === 0);
+      assert.equal(counter, 0);
 
       // Event should run and remove itself (only runs once).
       updateHandler.processEvent('status', 10);
-      assert(counter === 1);
+      assert.equal(counter, 1);
 
       // Since event not present, should do nothing.
       updateHandler.processEvent('status', 10);
-      assert(counter === 1);
+      assert.equal(counter, 1);
     });
 
     it('should not remove the handler if event does not happen', () => {
@@ -90,62 +84,62 @@ describe('UpdateHandler', () => {
         }
       }, (v) => v === 10);
 
-      assert(counter === 0);
+      assert.equal(counter, 0);
 
       updateHandler.processEvent('nope', 10);
-      assert(counter === 0);
+      assert.equal(counter, 0);
 
       updateHandler.processEvent('status', 10);
-      assert(counter === 1);
+      assert.equal(counter, 1);
     });
 
     it('should allow adding handlers for events that can be run, but not get removed immediately', () => {
       updateHandler.addHandler<number>('status', () => { counter += 1; }, (v) => v === 10);
 
-      assert(counter === 0);
+      assert.equal(counter, 0);
 
       updateHandler.processEvent('status', 0);
-      assert(counter === 1);
+      assert.equal(counter, 1);
 
       updateHandler.processEvent('status', -100);
-      assert(counter === 2);
+      assert.equal(counter, 2);
 
       updateHandler.processEvent('status', 1);
-      assert(counter === 3);
+      assert.equal(counter, 3);
 
       updateHandler.processEvent('status', 10);
-      assert(counter === 4);
+      assert.equal(counter, 4);
 
       updateHandler.processEvent('status', 10);
-      assert(counter === 4);
+      assert.equal(counter, 4);
     });
 
     it('should allow adding several handlers for events with different names', () => {
       updateHandler.addHandler<number>('status', () => { counter += 1; }, (v) => v === 10);
       updateHandler.addHandler<string>('job', () => { counter += 100; }, (v) => v === 'error');
 
-      assert(counter === 0);
+      assert.equal(counter, 0);
 
       updateHandler.processEvent('status', 0);
-      assert(counter === 1);
+      assert.equal(counter, 1);
 
       updateHandler.processEvent('job', -100);
-      assert(counter === 101);
+      assert.equal(counter, 101);
 
       updateHandler.processEvent('status', 1);
-      assert(counter === 102);
+      assert.equal(counter, 102);
 
       updateHandler.processEvent('job', 'error');
-      assert(counter === 202);
+      assert.equal(counter, 202);
 
       updateHandler.processEvent('status', 10);
-      assert(counter === 203);
+      assert.equal(counter, 203);
 
       updateHandler.processEvent('job', 'error');
-      assert(counter === 203);
+      assert.equal(counter, 203);
 
       updateHandler.processEvent('status', 10);
-      assert(counter === 203);
+      assert.equal(counter, 203);
     });
 
     it('should allow adding several handlers for events with the same name', () => {
@@ -154,85 +148,85 @@ describe('UpdateHandler', () => {
       updateHandler.addHandler<number>('status', () => { counter += 1; }, (v) => v === 10);
       updateHandler.addHandler<string>('status', () => { counter2 += 100; }, (v) => v === 'error');
 
-      assert(counter === 0);
-      assert(counter2 === 0);
+      assert.equal(counter, 0);
+      assert.equal(counter2, 0);
 
       updateHandler.processEvent('status', 0);
-      assert(counter === 1);
-      assert(counter2 === 100);
+      assert.equal(counter, 1);
+      assert.equal(counter2, 100);
 
       updateHandler.processEvent('status', 1);
-      assert(counter === 2);
-      assert(counter2 === 200);
+      assert.equal(counter, 2);
+      assert.equal(counter2, 200);
 
       updateHandler.processEvent('status', 'error');
-      assert(counter === 3);
-      assert(counter2 === 300);
+      assert.equal(counter, 3);
+      assert.equal(counter2, 300);
 
       updateHandler.processEvent('status', 'error');
-      assert(counter === 4);
-      assert(counter2 === 300);
+      assert.equal(counter, 4);
+      assert.equal(counter2, 300);
 
       updateHandler.processEvent('status', 10);
-      assert(counter === 5);
-      assert(counter2 === 300);
+      assert.equal(counter, 5);
+      assert.equal(counter2, 300);
 
       updateHandler.processEvent('status', 10);
-      assert(counter === 5);
-      assert(counter2 === 300);
+      assert.equal(counter, 5);
+      assert.equal(counter2, 300);
     });
 
     it('should allow for events to expire on their own after a certain amount of time', (done) => {
-      updateHandler.addHandler<number>('status', () => { counter += 1; }, (v) => v === 10, 25);
+      updateHandler.addHandler<number>('status', () => { counter += 1; }, (v) => v === 10, 10);
 
-      assert(counter === 0);
-
-      updateHandler.processEvent('status', 0);
-      assert(counter === 1);
+      assert.equal(counter, 0);
 
       updateHandler.processEvent('status', 0);
-      assert(counter === 2);
+      assert.equal(counter, 1);
+
+      updateHandler.processEvent('status', 0);
+      assert.equal(counter, 2);
 
       setTimeout(() => {
         counter += 100;
-        assert(counter === 102);
+        assert.equal(counter, 102);
 
         // Handler has expired already so this event should not increment the counter
         updateHandler.processEvent('status', 0);
-        assert(counter === 102);
+        assert.equal(counter, 102);
 
         done();
-      }, 50);
+      }, 20);
     });
 
     it('should allow for events to expire on their own after a certain amount of time, with more events for the same key', (done) => {
       let counter2 = 0;
 
-      updateHandler.addHandler<number>('status', () => { counter += 1; }, (v) => v === 10, 25);
+      updateHandler.addHandler<number>('status', () => { counter += 1; }, (v) => v === 10, 10);
       updateHandler.addHandler<string>('status', () => { counter2 += 1; }, (v) => v === 'error');
 
-      assert(counter === 0);
-      assert(counter2 === 0);
+      assert.equal(counter, 0);
+      assert.equal(counter2, 0);
 
       updateHandler.processEvent('status', 0);
-      assert(counter === 1);
-      assert(counter2 === 1);
+      assert.equal(counter, 1);
+      assert.equal(counter2, 1);
 
       updateHandler.processEvent('status', 0);
-      assert(counter === 2);
-      assert(counter2 === 2);
+      assert.equal(counter, 2);
+      assert.equal(counter2, 2);
 
       setTimeout(() => {
         counter += 100;
-        assert(counter === 102);
-        assert(counter2 === 2);
+        assert.equal(counter, 102);
+        assert.equal(counter2, 2);
 
         updateHandler.processEvent('status', 0);
-        assert(counter === 102);
-        assert(counter2 === 3);
+        assert.equal(counter, 102);
+        assert.equal(counter2, 3);
 
         done();
-      }, 50);
+      }, 20);
     });
 
     it('should not let handler expiry run if handler was removed from event', (done) => {
@@ -240,16 +234,16 @@ describe('UpdateHandler', () => {
         'status',
         (v) => { counter += v; },
         () => true,
-        40,
+        10,
         () => { counter += 1; },
       );
 
       updateHandler.processEvent('status', 0);
 
       setTimeout(() => {
-        assert(counter === 0, `counter should be 0 since expiry did not run, is ${counter}`);
+        assert.equal(counter, 0, `counter should be 0 since expiry did not run, is ${counter}`);
         done();
-      }, 50);
+      }, 20);
     });
   });
 
@@ -270,28 +264,28 @@ describe('UpdateHandler', () => {
         () => false,
       );
 
-      assert(statusCounter === 0);
-      assert(locationCounter === 0);
+      assert.equal(statusCounter, 0);
+      assert.equal(locationCounter, 0);
 
       updateHandler.processEvent('status', 1);
       updateHandler.processEvent('location', 1);
 
-      assert(statusCounter === 1);
-      assert(locationCounter === 1);
+      assert.equal(statusCounter, 1);
+      assert.equal(locationCounter, 1);
 
       statusHandler.remove();
       updateHandler.processEvent('status', 1);
       updateHandler.processEvent('location', 1);
 
-      assert(statusCounter === 1);
-      assert(locationCounter === 2);
+      assert.equal(statusCounter, 1);
+      assert.equal(locationCounter, 2);
 
       locationHandler.remove();
       updateHandler.processEvent('status', 1);
       updateHandler.processEvent('location', 1);
 
-      assert(statusCounter === 1);
-      assert(locationCounter === 2);
+      assert.equal(statusCounter, 1);
+      assert.equal(locationCounter, 2);
     });
 
     it('should not let handler expiry run if handler was deleted', (done) => {
@@ -299,21 +293,21 @@ describe('UpdateHandler', () => {
         'status',
         (v) => { counter += v; },
         () => true,
-        40,
+        10,
         () => { counter += 1; },
       );
 
       handler.remove();
 
       setTimeout(() => {
-        assert(counter === 0, `counter should be 0 since expiry did not run, is ${counter}`);
+        assert.equal(counter, 0, `counter should be 0 since expiry did not run, is ${counter}`);
         assert.deepEqual(
           updateHandler.handlers.get('status'),
           [],
           `array in "status" is not empty, is ${updateHandler.handlers.get('status')}`,
         );
         done();
-      }, 50);
+      }, 20);
     });
   });
 });
